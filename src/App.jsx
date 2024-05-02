c60dd26b9acb359b1a2afd562ed561ac2b0c216b
Abdur Rofi <rofim58@gmail.com>
2024-05-02 22:37:28 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,9 @@
+import {
+  NavigationMenu,
+  NavigationMenuList,
+  NavigationMenuItem,
+} from "@/components/ui/navigation-menu";
+
 const menu = [
   {
     name: "Beranda",
@@ -20,24 +26,17 @@ const menu = [
 function App() {
   return (
     <>
-      <nav class="fixed top-0 w-full bg-white shadow-md z-10">
-        <div class="container mx-auto flex items-center justify-between py-4">
-          <a href="/" class="text-xl font-bold text-gray-800">
-            <span class="text-blue-500">Sinergi</span> Karya Solutif
-          </a>
-          <ul class="flex space gap-4">
-            {menu.map((item) => (
-              <li>
-                <a href={item.url} class="text-gray-700 hover:text-gray-900">
-                  {item.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </nav>
+      <NavigationMenu>
+        <NavigationMenuList className="container">
+          <NavigationMenuItem>
+            <a href="/">
+              <img src="/logo.png" alt="Sinergi Karya Solutif" class="h-20" />
+            </a>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
       <main>
-        <section class="flex flex-col items-center justify-center h-screen">
+        <section class="flex flex-col items-center justify-center min-h-screen">
           <div className="container mx-auto text-center">
             <p className="text-4xl font-bold text-gray-800">
               Sinergi Karya Solutif
@@ -49,21 +48,6 @@ function App() {
           </div>
         </section>
       </main>
-      <footer class="absolute bottom-0 w-full text-center font-sans font-light text-sm pb-4 text-gray-500">
-        <div class="container mx-auto">
-          <p>
-            Made with ❤️ by{" "}
-            <a
-              href="https://jawa.cloud/"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="underline hover:text-gray-700 font-bold"
-            >
-              Jawara Cloud
-            </a>
-          </p>
-        </div>
-      </footer>
     </>
   );
 }
